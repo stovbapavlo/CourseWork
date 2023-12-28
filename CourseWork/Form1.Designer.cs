285e1ae6c64c8bc0ef949efbbb6da9b70412cbe9
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-28 20:10:33 +0200

    Add project files.

---
+++ CourseWork/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿using System.Diagnostics;
+
+namespace CourseWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            button2 = new Button();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 19;
+            listBox1.Location = new Point(149, 57);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(264, 270);
+            listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(171, 358);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(209, 26);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(171, 422);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(209, 26);
+            textBox2.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(699, 103);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(233, 26);
+            dateTimePicker1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(699, 167);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(233, 26);
+            dateTimePicker2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(697, 452);
+            button2.Name = "button2";
+            button2.Size = new Size(232, 66);
+            button2.TabIndex = 6;
+            button2.Text = "calculate";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(689, 316);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(240, 26);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(700, 246);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(233, 26);
+            textBox4.TabIndex = 8;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(718, 405);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(81, 23);
+            radioButton1.TabIndex = 9;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Хвороба";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(837, 405);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(72, 23);
+            radioButton2.TabIndex = 10;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Декрет";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(697, 166);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(234, 27);
+            comboBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(577, 253);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 19);
+            label1.TabIndex = 12;
+            label1.Text = "Кількість днів:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(656, 109);
+            label2.Name = "label2";
+            label2.Size = new Size(17, 19);
+            label2.TabIndex = 13;
+            label2.Text = "З";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(656, 173);
+            label3.Name = "label3";
+            label3.Size = new Size(27, 19);
+            label3.TabIndex = 14;
+            label3.Text = "До";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(505, 319);
+            label4.Name = "label4";
+            label4.Size = new Size(178, 19);
+            label4.TabIndex = 15;
+            label4.Text = "Загальна сума нарахувань:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(945, 319);
+            label5.Name = "label5";
+            label5.Size = new Size(33, 19);
+            label5.TabIndex = 16;
+            label5.Text = "грн.";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(89, 361);
+            label6.Name = "label6";
+            label6.Size = new Size(55, 19);
+            label6.TabIndex = 17;
+            label6.Text = "Посада";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(51, 429);
+            label7.Name = "label7";
+            label7.Size = new Size(104, 19);
+            label7.TabIndex = 18;
+            label7.Text = "Місячка ставка";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(617, 173);
+            label8.Name = "label8";
+            label8.Size = new Size(66, 19);
+            label8.TabIndex = 19;
+            label8.Text = "Кількість:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(215, 26);
+            label9.Name = "label9";
+            label9.Size = new Size(133, 19);
+            label9.TabIndex = 20;
+            label9.Text = "Список працівників";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(776, 26);
+            label10.Name = "label10";
+            label10.Size = new Size(82, 19);
+            label10.TabIndex = 21;
+            label10.Text = "Розрахунок";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(386, 425);
+            label11.Name = "label11";
+            label11.Size = new Size(33, 19);
+            label11.TabIndex = 22;
+            label11.Text = "грн.";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1084, 569);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(button2);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Button button2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private ComboBox comboBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+    }
+}
---
+++ CourseWork/Form1.cs
@@ -0,0 +1,271 @@
+using System;
+using System.Data;
+using System.IO;
+using ExcelDataReader;
+using System.Windows.Forms;
+using System.Text;
+using System.Globalization;
+
+namespace CourseWork
+{
+    public partial class Form1 : Form
+    {
+        DataTable table = new DataTable();
+        private int selectedSalary;
+        private string selectedEmployeeName = string.Empty;
+        private double averageDailySalary = 0;
+        private int totalSickDays = 0;
+        private DateTime startDate;
+        private double discountPercentage = 0.0;
+
+
+        public Form1()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            InitializeComponent();
+
+            label3.Visible = false;
+            label2.Visible = false;
+            label8.Visible = false;
+
+            dateTimePicker1.Visible = false;
+            dateTimePicker2.Visible = false;
+            comboBox1.Visible = false;
+
+            radioButton1.CheckedChanged += radioButton_CheckedChanged;
+            radioButton2.CheckedChanged += radioButton_CheckedChanged;
+
+            comboBox1.Items.AddRange(new object[] { 126, 140, 180 });
+
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            LoadDataAndPopulateListBox();
+        }
+
+        private void LoadDataAndPopulateListBox()
+        {
+            string filePath = @"D:\курсова\CourseWork\CourseWork\bin\Debug\personal.xlsx";
+
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet();
+                    table = result.Tables[0];
+                }
+            }
+
+            foreach (DataColumn column in table.Columns)
+            {
+                Console.WriteLine(column.ColumnName);
+            }
+
+            listBox1.DataSource = table;
+            listBox1.ValueMember = table.Columns[0].ColumnName;
+            listBox1.DisplayMember = table.Columns[0].ColumnName;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                DataRowView selectedRow = (DataRowView)listBox1.SelectedItem;
+
+                string fullName = selectedRow.Row[0].ToString();
+
+                DataRow[] filteredRows = table.Select($"{table.Columns[0].ColumnName} = '{fullName}'");
+
+                if (filteredRows.Length > 0)
+                {
+
+                    DateTime startDate = DateTime.ParseExact(filteredRows[0][3].ToString(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+
+                    discountPercentage = CalculateDiscountPercentage(startDate);
+
+                    double monthlySalary = double.Parse(filteredRows[0][2].ToString());
+
+
+                    averageDailySalary = (monthlySalary * 12) / 365;
+                    textBox1.Text = filteredRows[0][1].ToString();
+                    textBox2.Text = filteredRows[0][2].ToString();
+
+                    selectedEmployeeName = fullName;
+                    selectedSalary = int.Parse(filteredRows[0][2].ToString());
+                }
+            }
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                // Показати dateTimePicker1 і dateTimePicker2
+                dateTimePicker1.Visible = true;
+                dateTimePicker2.Visible = true;
+
+                label3.Visible = true;
+                label2.Visible = true;
+                // Приховати comboBox1
+                comboBox1.Visible = false;
+            }
+            else if (radioButton2.Checked)
+            {
+                // Приховати dateTimePicker1 і dateTimePicker2
+                dateTimePicker1.Visible = true;
+                dateTimePicker2.Visible = false;
+
+                label2.Visible = true;
+                label8.Visible = true;
+
+                // Показати comboBox1
+                comboBox1.Visible = true;
+            }
+
+            button2.Visible = radioButton1.Checked || radioButton2.Checked;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(selectedEmployeeName))
+                return;
+
+            // Очистіть попередні результати
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+            int totalSalary = CalculateTotalSalary(selectedEmployeeName);
+
+            int daysDifference;
+
+            if (radioButton2.Checked && comboBox1.SelectedItem is int selectedDays)
+            {
+                // Обчислення для radioButton2 (декретні)
+                daysDifference = selectedDays;
+                totalSickDays = selectedDays;
+            }
+            else if (!string.IsNullOrWhiteSpace(textBox4.Text) && int.TryParse(textBox4.Text, out int manualDays))
+            {
+                // Перевірка, чи введена кількість днів більше або рівною нулю
+                if (manualDays <= 0)
+                {
+                    MessageBox.Show("Кількість днів повинна бути більше або рівною нулю.");
+                    return; // Завершити виконання методу, якщо є помилка
+                }
+
+                // Обчислення для radioButton1 (лікарняні) або введено вручну
+                daysDifference = manualDays;
+                totalSickDays = manualDays;
+            }
+            else
+            {
+                // Додана умова для перевірки дат
+                if (dateTimePicker1.Value > dateTimePicker2.Value)
+                {
+                    MessageBox.Show("Дата початку не може бути пізнішою, ніж дата кінця, або співпадати з нею.");
+                    textBox3.Text = string.Empty;
+                    return;
+                }
+
+                // Обчислення для radioButton1 (лікарняні) з dateTimePicker1 і dateTimePicker2
+                daysDifference = (int)(dateTimePicker2.Value - dateTimePicker1.Value).TotalDays;
+
+                // Перевірка, чи введена кількість днів більше або рівною нулю
+                if (daysDifference <= 0)
+                {
+                    MessageBox.Show("Кількість днів повинна бути більше або рівною нулю.");
+                    return; // Завершити виконання методу, якщо є помилка
+                }
+
+                totalSickDays = daysDifference;
+            }
+
+            textBox3.Text = $"{selectedEmployeeName}: {totalSalary}$.";
+
+            if (radioButton1.Checked)
+            {
+                // Обчислення лікарняних
+                double sicknessAllowance = CalculateSicknessAllowance(averageDailySalary, daysDifference, discountPercentage);
+                textBox3.Text = $"{sicknessAllowance}";
+                textBox4.Text = $"{totalSickDays}";
+            }
+            else if (radioButton2.Checked)
+            {
+                // Обчислення декретних
+                double dailySalary = totalSalary / 365;
+
+                double maternityAllowance = CalculateMaternityAllowance(dailySalary, daysDifference);
+                textBox3.Text = $"{maternityAllowance}";
+                textBox4.Text = $"{totalSickDays}";
+            }
+        }
+
+
+        private double CalculateDiscountPercentage(DateTime startDate)
+        {
+            int yearsOfWork = (int)Math.Round((DateTime.Now - startDate).TotalDays / 365);
+
+            // Визначити відсоток знижки в залежності від стажу
+            if (yearsOfWork < 3)
+                return 0.50;
+            else if (yearsOfWork < 5)
+                return 0.60;
+            else if (yearsOfWork < 7)
+                return 0.70;
+            else
+                return 1.0;
+        }
+
+
+
+
+        private double CalculateMaternityAllowance(double averageDailySalary, int maternityLeaveDays)
+        {
+            double maternityAllowance = averageDailySalary * maternityLeaveDays;
+            return Math.Round(maternityAllowance, 2);
+        }
+
+
+
+        private double CalculateSicknessAllowance(double averageDailySalary, int totalSickDays, double discountPercentage)
+        {
+            // Розрахунок денної виплати
+            double dailySalary = averageDailySalary * totalSickDays * discountPercentage;
+
+            // Розрахунок відшкодування від роботодавця
+            /*            double employerCoverage = dailySalary * totalSickDays;*/
+
+            // Округлення результату
+            return Math.Round(dailySalary, 2);
+        }
+
+
+
+
+        private int CalculateTotalSalary(string employeeName)
+        {
+            DataRow[] filteredRows = table.Select($"{table.Columns[0].ColumnName} = '{employeeName}'");
+
+            if (filteredRows.Length > 0)
+            {
+                if (int.TryParse(filteredRows[0][2].ToString(), out int monthlySalary))
+                {
+                    int totalSalary = monthlySalary * 12;
+                    return totalSalary;
+                }
+                else
+                {
+                    MessageBox.Show("Помилка при отриманні заробітної плати працівника.");
+                }
+            }
+
+            return 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+        }
+    }
+}
